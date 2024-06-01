bc467f05d34c11c50f9e1eb666f5941c38fac219
manishquare <ramansung89@gmail.com>
2024-06-01 12:21:58 +0530

    React web site commit from local repository

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class Header extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="container">
+        <Row>
+          <h1 className="col">h</h1>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>
+);
